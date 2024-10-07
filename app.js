39306f68646085ab3bcb9e1663e3ae84cc4e5b64
Blake Cromar <blake.cromar@icloud.com>
2024-10-07 20:31:29 +0200

    Added three comparison methods to the Dino constructor

--- app.js
+++ app.js
@@ -7,6 +7,33 @@
         this.diet = diet;
     }
 
+    // Dino Compare Method 1: Compare Height
+    Dino.prototype.compareHeight = function(human) {
+        const relativeDiffHeight = this.calcRelativeDiff(human.height, this.height);
+        return `${human.name} is ${relativeDiffHeight.toFixed(2)}% different in height than a(n) ${this.species}.`;
+    };
+
+    // Dino Compare Method 2: Compare Weight
+    Dino.prototype.compareWeight = function(human) {
+        const relativeDiffWeight = this.calcRelativeDiff(human.weight, this.weight);
+        return `${human.name} is ${relativeDiffWeight.toFixed(2)}% different in weight than a(n) ${this.species}.`;
+    };
+
+    // Dino Compare Method 3: Compare Diet
+    Dino.prototype.compareDiet = function(human) {
+        return human.diet.toLowerCase() === this.diet.toLowerCase()
+            ? `${human.name} has the same diet as a(n) ${this.species}.`
+            : `${human.name} has a different diet than a(n) ${this.species}.`;
+    };
+
+    // Relative Difference Function
+    Dino.prototype.calcRelativeDiff = function(humanMeasurement, dinoMeasurement) {
+        const difference = Math.abs(humanMeasurement - dinoMeasurement);
+        const average = (humanMeasurement + dinoMeasurement) / 2;
+
+        return (difference / average) * 100; // Return the relative difference as a percentage
+    };
+
     // Create Dino Objects
 
 
@@ -15,16 +42,7 @@
     // Use IIFE to get human data from form
 
 
-    // Create Dino Compare Method 1
-    // NOTE: Weight in JSON file is in lbs, height in inches. 
 
-    
-    // Create Dino Compare Method 2
-    // NOTE: Weight in JSON file is in lbs, height in inches.
-
-    
-    // Create Dino Compare Method 3
-    // NOTE: Weight in JSON file is in lbs, height in inches.
 
 
     // Generate Tiles for each Dino in Array
